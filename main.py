b956881fcf585d0d6f29c41771b87f2c8390692c
Jake Therien <Jake.Therien@pm.me>
2021-05-08 19:16:44 -0400

    Clean formatting. Rearrange code for better flow

--- main.py
+++ main.py
@@ -1,146 +1,142 @@
-import csv
 import os
+import csv
 import hashlib
-import dropbox
 import shutil
-import py7zr
-import time
+from tempfile import NamedTemporaryFile
 from tqdm import tqdm
+import py7zr
+import dropbox
 
 from libs.config import get_config
 
 # future enhancements
 # - tqdm async for when generating zip archive
 # - Write hash values to .csv file as you go rather than waiting until the end
+# - Chunk excessively big libraries
 
 BLOCKSIZE = 4 * 1024 * 1024
 
 config = get_config()
+tmp = NamedTemporaryFile('w+t', newline='', delete=False)
 backup_list = config['backup inventory']
 file_format = config['format']
-
-# establish a connection to the Dropbox API
-dbx = dropbox.Dropbox(config['token'], timeout=None)
+lines = [['Path','Archive','Last Hash']]
 
 # register 7zip as an archive format in shutil
 shutil.register_archive_format('7z', py7zr.pack_7zarchive, description='7zip archive')
 
-with open(backup_list, 'r', newline='') as f:
+# establish a connection to the Dropbox API
+dbx = dropbox.Dropbox(config['token'], timeout=None)
+dropbox_files = [e.name for e in dbx.files_list_folder('/Backups/').entries]
 
-    reader = csv.reader(f, delimiter=',')
-
-    lines = [['Path','Archive','Last Hash']]
+with open(backup_list, 'r', newline='') as f, tmp:
     
+    reader = csv.reader(f, delimiter=',', quotechar='"')
+    writer = csv.writer(tmp, delimiter=',', quotechar='"')
+    
+    next(reader)
+
     for line in reader:
         
-        if os.path.isdir(line[0]):
-            
-            process_backup = True
+        file_name_ext = f'{line[1]}.{file_format}'
+        file_path = f'files-to-upload\\{line[1]}'
+        file_path_ext = f'{file_path}.{file_format}'
+        dropbox_path_ext = f'/Backups/{file_name_ext}'
 
-            archive_name = f'files-to-upload\\{line[1]}'
-            archive_path = f'{archive_name}.{file_format}'
+        if file_name_ext in dropbox_files:
             
-            dropbox_path = f'/Backups/{line[1]}.{file_format}'
-            
-            if f'{line[1]}.{file_format}' in [e.name for e in dbx.files_list_folder('/Backups/').entries]:
-                dropbox_hash = dbx.files_alpha_get_metadata(dropbox_path).content_hash
-            else:
-                dropbox_hash = None
+            dropbox_content_hash =  dbx.files_alpha_get_metadata(dropbox_path_ext).content_hash
 
-            # check if dropbox_hash matches what is in the database before archiving the file
-            if line[2] == dropbox_hash:
-                
-                print('Recorded hash matches Dropbox hash. Skipping.')
-            
-            else:
-                
-                print(f'Creating a {file_format} archive for: {line[0]}')
-                shutil.make_archive(archive_name, file_format, line[0])
-                archive_size = os.path.getsize(archive_path)
+        else:
 
-                print(f'Finished creating {file_format} archive')
-            
-                # Create a progress bar for reading the hash of the archive
-                progress = tqdm(range(archive_size), 'Verifying archive changes', unit='B', unit_scale=True, unit_divisor=1024)
-            
-                with open(archive_path, 'rb') as a:
+            dropbox_content_hash = None
 
-                    hasher = hashlib.sha256()
+        if (os.path.isdir(line[0])) & (line[2] != dropbox_content_hash):
+            
+            print(f'Creating a {file_format} archive for: {line[0]}')
+            shutil.make_archive(file_path, file_format, line[0])
+            file_size = os.path.getsize(file_path_ext)
+            print(f'Finished creating {file_format} archive')
+            
+            progress = tqdm(range(file_size), 'Verifying archive changes', unit='B', unit_scale=True, unit_divisor=1024)
+            
+            # Get hash value of the local archive
+            with open(file_path_ext, 'rb') as a:
+
+                hasher = hashlib.sha256()
+                buffer = a.read(BLOCKSIZE)
+                progress.update(len(buffer))
+
+                while len(buffer) > 0:
+                        
+                    hasher.update(hashlib.sha256(buffer).digest())
                     buffer = a.read(BLOCKSIZE)
                     progress.update(len(buffer))
 
-                    while len(buffer) > 0:
-                        
-                        hasher.update(hashlib.sha256(buffer).digest())
-                        buffer = a.read(BLOCKSIZE)
-                        progress.update(len(buffer))
+                local_hash = hasher.hexdigest()
 
-                    local_hash = hasher.hexdigest()
-
-                if local_hash == dropbox_hash:
+            # if the local archive matches Dropbox at this stage, that 
+            # means the backup database had an incorrect hash value stored
+            if local_hash == dropbox_content_hash:
                     
-                    print('Generated archive hash matches Dropbox hash. Skipping.')
-                    line[2] = local_hash
+                print('Generated archive hash matches Dropbox hash. Skipping.')
                 
-                else:
+            else:
 
-                    print('Uploading to Dropbox.')
-
-                    with open(archive_path, 'rb') as a:
+                print('Uploading to Dropbox.')
+                with open(file_name_ext, 'rb') as a:
                         
-                        # Check if the zip archive is less than 150mb
-                        if archive_size <= BLOCKSIZE:
-
-                            dbx.files_upload(a.read(), dropbox_path)
-
-                        # read more:
-                        # https://www.dropboxforum.com/t5/Dropbox-API-Support-Feedback/How-to-upload-files-in-batch/td-p/434689
-                        else:
+                    progress = tqdm(range(file_size), 'Uploading large file', unit='B', unit_scale=True, unit_divisor=1024)
                             
-                            progress = tqdm(range(archive_size), 'Uploading large file', unit='B', unit_scale=True, unit_divisor=1024)
+                    upload_session_start_result = dbx.files_upload_session_start(a.read(BLOCKSIZE))
                             
-                            upload_session_start_result = dbx.files_upload_session_start(a.read(BLOCKSIZE))
-                            
-                            cursor = dropbox.files.UploadSessionCursor(
-                                session_id=upload_session_start_result.session_id,
-                                offset=a.tell()
-                                )
+                    cursor = dropbox.files.UploadSessionCursor(
+                        session_id=upload_session_start_result.session_id,
+                        offset=a.tell()
+                        )
 
-                            commit = dropbox.files.CommitInfo(path=dropbox_path)
+                    commit = dropbox.files.CommitInfo(path=dropbox_path_ext)
 
-                            while a.tell() <= archive_size:
+                    while a.tell() <= file_size:
                                 
-                                upload_buffer = a.read(BLOCKSIZE)
+                        upload_buffer = a.read(BLOCKSIZE)
                                 
-                                if ((archive_size - a.tell()) <= BLOCKSIZE):
+                        if ((file_size - a.tell()) <= BLOCKSIZE):
                                     
-                                    print(dbx.files_upload_session_finish(upload_buffer,cursor,commit))
-                                    progress.update(len(upload_buffer))
-                                    print('Upload complete')
+                            print(dbx.files_upload_session_finish(upload_buffer,cursor,commit))
+                            progress.update(len(upload_buffer))
+                            print('Upload complete')
 
-                                    break
+                            break
                             
-                                else:
+                        else:
                                 
-                                    dbx.files_upload_session_append_v2(upload_buffer,cursor)
-                                    progress.update(len(upload_buffer))
-                                    cursor.offset = a.tell()
+                            dbx.files_upload_session_append_v2(upload_buffer,cursor)
+                            progress.update(len(upload_buffer))
+                            cursor.offset = a.tell()
 
-                        print('Updating hash value')
-                        line[2] = local_hash
+                print('Verifying hash values')
 
+                dropbox_content_hash =  dbx.files_alpha_get_metadata(dropbox_path_ext).content_hash
+                
+                if local_hash == dropbox_content_hash:
+                    print('Hash values verified')
+                else:
+                    print('Hash values do not match')
+                
+            line[2] = local_hash
 
-            # remove file after check and / or upload is complete
-            os.remove(f'{archive_name}.{file_format}')
+        # remove file after check and / or upload is complete
+        if os.path.isdir(file_path_ext):
+            os.remove(file_path_ext)
 
-            # overwrite the backup inventory with the latest file hash
-            lines.append(line)
+        # overwrite the backup inventory with the latest file hash
+        lines.append(line)
+
+    for line in writer:
+        
+        writer.writerows(lines)
+
+shutil.move(tmp.name, backup_list)
 
 dbx.close()
-
-# push updated hash values to the backup inventory
-with open(backup_list, 'w', newline='') as f:
-    
-    writer = csv.writer(f, delimiter=',')
-    
-    writer.writerows(lines)
